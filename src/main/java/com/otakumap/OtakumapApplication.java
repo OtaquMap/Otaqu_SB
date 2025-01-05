4f2ad271ee52c1f97113e2f3c0999c5bfaf266bf
haerxeong <haeryeong04@naver.com>
2025-01-05 14:40:44 +0900

    first commit

---
+++ src/main/java/com/otakumap/OtakumapApplication.java
@@ -0,0 +1,13 @@
+package com.otakumap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtakumapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtakumapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otakumap/OtakumapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otakumap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtakumapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
