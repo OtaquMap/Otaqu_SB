a97094b25604e4ad3282d513797871f3f6f70e2b
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-06 00:10:38 +0900

    Feat: 기본 응답 형식 설계, 공통 예외 처리

---
+++ src/main/java/com/otakumap/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.otakumap.apiPayload.exception;
+
+import com.otakumap.apiPayload.code.BaseErrorCode;
+import com.otakumap.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
