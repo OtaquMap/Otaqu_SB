eafa1d3219822a25315b244501cdb290bbc1e42b
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-21 07:31:03 +0900

    Feat: 후기 엔티티와 Route 1:1 관계 추가

--- src/main/java/com/otakumap/domain/place_review/entity/PlaceReview.java
+++ src/main/java/com/otakumap/domain/place_review/entity/PlaceReview.java
@@ -2,6 +2,7 @@ package com.otakumap.domain.place_review.entity;
 
 import com.otakumap.domain.image.entity.Image;
 import com.otakumap.domain.place.entity.Place;
+import com.otakumap.domain.route.entity.Route;
 import com.otakumap.domain.user.entity.User;
 import com.otakumap.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -41,4 +42,8 @@ public class PlaceReview extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "place_id", nullable = false)
     private Place place;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id", referencedColumnName = "id")
+    private Route route;
 }
