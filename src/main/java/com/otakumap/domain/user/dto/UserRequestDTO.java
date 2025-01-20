f4885fc749d7896e1e9d38a2b031ebe23bcbfdc1
haerxeong <haeryeong04@naver.com>
2025-01-20 19:48:46 +0900

    Feat: request dto에 조건 추가

--- src/main/java/com/otakumap/domain/user/dto/UserRequestDTO.java
+++ src/main/java/com/otakumap/domain/user/dto/UserRequestDTO.java
@@ -1,10 +1,12 @@
 package com.otakumap.domain.user.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class UserRequestDTO {
     @Getter
     public static class UpdateNicknameDTO {
+        @NotNull
         private String nickname;
     }
 }
