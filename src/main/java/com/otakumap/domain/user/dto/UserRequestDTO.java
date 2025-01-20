5589fda3f8bc8279795d069b5499ea46bbc694fb
haerxeong <haeryeong04@naver.com>
2025-01-20 19:50:58 +0900

    Feat: 닉네임 길이 유효성 검사 추가

--- src/main/java/com/otakumap/domain/user/dto/UserRequestDTO.java
+++ src/main/java/com/otakumap/domain/user/dto/UserRequestDTO.java
@@ -1,12 +1,14 @@
 package com.otakumap.domain.user.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 public class UserRequestDTO {
     @Getter
     public static class UpdateNicknameDTO {
         @NotNull
+        @Size(min = 1, max = 20, message = "닉네임은 1자 이상 20자 이하로 입력해주세요.")
         private String nickname;
     }
 }
