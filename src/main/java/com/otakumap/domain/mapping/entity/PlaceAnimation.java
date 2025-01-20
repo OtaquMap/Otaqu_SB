e1591cbec6b1839dbdbf4dd4c443efd027efa0e9
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-20 17:10:31 +0900

    Feat: Animation, EventAnimation, PlaceAnimation 엔티티 추가

---
+++ src/main/java/com/otakumap/domain/mapping/entity/PlaceAnimation.java
@@ -0,0 +1,27 @@
+package com.otakumap.domain.mapping.entity;
+
+import com.otakumap.domain.animation.entity.Animation;
+import com.otakumap.domain.place.entity.Place;
+import com.otakumap.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PlaceAnimation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "animation_id")
+    private Animation animation;
+}
