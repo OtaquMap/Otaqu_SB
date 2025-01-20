47b300790c9dab7ac3eaea0bdb0794e693b5f425
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-21 07:33:06 +0900

    Feat: 루트 저장 API 구현

---
+++ src/main/java/com/otakumap/domain/route_item/enums/ItemType.java
@@ -0,0 +1,6 @@
+package com.otakumap.domain.route_item.enums;
+
+public enum ItemType {
+    PLACE,
+    EVENT
+}
---
+++ src/main/java/com/otakumap/domain/route_like/enums/Status.java
@@ -0,0 +1,6 @@
+package com.otakumap.domain.route_like.enums;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE
+}
