78c27148c0555f9d80113ce98b79d2d520162cd6
tl1l1l1s <shj721553@gmail.com>
2025-01-21 16:28:24 +0900

    Feat: 이벤트 없을 경우 에러 처리

--- src/main/java/com/otakumap/domain/event_like/service/EventLikeCommandServiceImpl.java
+++ src/main/java/com/otakumap/domain/event_like/service/EventLikeCommandServiceImpl.java
@@ -1,9 +1,12 @@
 package com.otakumap.domain.event_like.service;
 
+import com.otakumap.domain.event.entity.Event;
 import com.otakumap.domain.event.repository.EventRepository;
 import com.otakumap.domain.event_like.entity.EventLike;
 import com.otakumap.domain.event_like.repository.EventLikeRepository;
 import com.otakumap.domain.user.entity.User;
+import com.otakumap.global.apiPayload.code.status.ErrorStatus;
+import com.otakumap.global.apiPayload.exception.handler.EventHandler;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,9 +24,11 @@ public class EventLikeCommandServiceImpl implements EventLikeCommandService {
 
     @Override
     public void addEventLike(User user, Long eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventHandler(ErrorStatus.EVENT_NOT_FOUND));
         eventLikeRepository.save(
                 EventLike.builder()
-                        .event(eventRepository.getReferenceById(eventId))
+                        .event(event)
                         .user(user)
                         .isFavorite(true)
                         .build()
