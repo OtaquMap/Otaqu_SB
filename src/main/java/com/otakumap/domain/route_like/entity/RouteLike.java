08fb53761e9e19667f5a03d8d7b45da3347f4314
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-01-29 01:12:35 +0900

    Fix: @Setter 추가함으로써 제목 수정 안 되던 오류 고침 (다른 방법으로는, setName 메소드에 this.name = name; 넣어주어야 함)

--- src/main/java/com/otakumap/domain/route_like/entity/RouteLike.java
+++ src/main/java/com/otakumap/domain/route_like/entity/RouteLike.java
@@ -23,6 +23,7 @@ public class RouteLike extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(length = 50, nullable = false)
     private String name;
 
@@ -39,6 +40,4 @@ public class RouteLike extends BaseEntity {
     @ColumnDefault("false")
     private Boolean isFavorite;
 
-    public void setName(String name) {
-    }
 }
