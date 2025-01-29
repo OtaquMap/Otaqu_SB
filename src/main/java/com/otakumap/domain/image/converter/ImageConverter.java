ef226e5768b696c7d649adeeee05c69f8d7c207a
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-30 07:05:37 +0900

    Feat: 사진이 없는 경우 null 반환하게 수정

--- src/main/java/com/otakumap/domain/image/converter/ImageConverter.java
+++ src/main/java/com/otakumap/domain/image/converter/ImageConverter.java
@@ -6,6 +6,11 @@ import com.otakumap.domain.image.entity.Image;
 public class ImageConverter {
 
     public static ImageResponseDTO.ImageDTO toImageDTO(Image image) {
+
+        if(image == null) {
+            return new ImageResponseDTO.ImageDTO();
+        }
+
         return ImageResponseDTO.ImageDTO.builder()
                 .id(image.getId())
                 .uuid(image.getUuid())
