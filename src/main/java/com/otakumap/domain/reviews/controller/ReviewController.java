bb902c775ee720d7fd174dc101d8f17026a21984
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-01-31 23:38:33 +0900

    Rename: ReviewSearchController->ReviewController 수정

--- src/main/java/com/otakumap/domain/reviews/controller/ReviewController.java
+++ src/main/java/com/otakumap/domain/reviews/controller/ReviewController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class ReviewSearchController {
+public class ReviewController {
 
     private final ReviewQueryService reviewQueryService;
 
