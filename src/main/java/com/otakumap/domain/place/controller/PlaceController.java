a40944cde8dab831ab610d76383fce7a3b9ddd21
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-02 08:28:43 +0900

    Refactor: PlaceController 필요 없는 코드 삭제

--- src/main/java/com/otakumap/domain/place/controller/PlaceController.java
+++ src/main/java/com/otakumap/domain/place/controller/PlaceController.java
@@ -1,24 +1,12 @@
 package com.otakumap.domain.place.controller;
 
-import com.otakumap.domain.place.dto.PlaceResponseDTO;
-import com.otakumap.domain.place.service.GooglePlacesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class PlaceController {
-
-    private final GooglePlacesService googlePlacesService;
-
-
-    @GetMapping("/places/search")
-    public PlaceResponseDTO searchPlaces(@RequestParam String query) {
-        return googlePlacesService.searchPlaces(query);
-    }
 
 }
