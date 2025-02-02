c60eb699669e1080a6caa96133e8fc3513e2e2af
mk-star <ssunn0812@naver.com>
2025-02-02 15:03:42 +0900

    Feat: PlaceLike의 유효성 검사를 ExistPlaceLike, ExistPlaceListLike로 분리

--- src/main/java/com/otakumap/global/validation/annotation/ExistPlaceListLike.java
+++ src/main/java/com/otakumap/global/validation/annotation/ExistPlaceListLike.java
@@ -1,17 +1,17 @@
 package com.otakumap.global.validation.annotation;
 
-import com.otakumap.global.validation.validator.PlaceLikeExistValidator;
+import com.otakumap.global.validation.validator.PlaceListLikeExistValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = PlaceLikeExistValidator.class)
+@Constraint(validatedBy = PlaceListLikeExistValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ExistPlaceLike {
-    String message() default "유효하지 않은 명소 ID 입니다.";
+public @interface ExistPlaceListLike {
+    String message() default "유효하지 않은 명소 ID가 포함되어 있습니다.";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
