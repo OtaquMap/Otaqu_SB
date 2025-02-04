7ec2c3ba6395b06955a08bd696837abc013605a3
mk-star <ssunn0812@naver.com>
2025-02-04 22:23:18 +0900

    Feat: 커스텀 루트 저장 API 구현

--- src/main/java/com/otakumap/domain/route_like/service/RouteLikeCommandServiceImpl.java
+++ src/main/java/com/otakumap/domain/route_like/service/RouteLikeCommandServiceImpl.java
@@ -1,13 +1,19 @@
 package com.otakumap.domain.route_like.service;
 
+import com.otakumap.domain.place.entity.Place;
+import com.otakumap.domain.place.repository.PlaceRepository;
+import com.otakumap.domain.route.converter.RouteConverter;
 import com.otakumap.domain.route.entity.Route;
 import com.otakumap.domain.route.repository.RouteRepository;
+import com.otakumap.domain.route_item.converter.RouteItemConverter;
+import com.otakumap.domain.route_item.entity.RouteItem;
 import com.otakumap.domain.route_like.converter.RouteLikeConverter;
 import com.otakumap.domain.route_like.dto.RouteLikeRequestDTO;
 import com.otakumap.domain.route_like.entity.RouteLike;
 import com.otakumap.domain.route_like.repository.RouteLikeRepository;
 import com.otakumap.domain.user.entity.User;
 import com.otakumap.global.apiPayload.code.status.ErrorStatus;
+import com.otakumap.global.apiPayload.exception.handler.PlaceHandler;
 import com.otakumap.global.apiPayload.exception.handler.RouteHandler;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -15,14 +21,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RouteLikeCommandServiceImpl implements RouteLikeCommandService {
-
     private final RouteLikeRepository routeLikeRepository;
     private final RouteRepository routeRepository;
     private final EntityManager entityManager;
+    private final PlaceRepository placeRepository;
 
     @Override
     public void saveRouteLike(User user, Long routeId) {
@@ -66,4 +73,18 @@ public class RouteLikeCommandServiceImpl implements RouteLikeCommandService {
         return routeLikeRepository.save(routeLike);
     }
 
+    @Override
+    public void saveCustomRouteLike(RouteLikeRequestDTO.SaveCustomRouteLikeDTO request, User user) {
+        List<RouteItem> routeItems = request.getRouteItems()
+                .stream()
+                .map(routeItem -> {
+                    Place place = placeRepository.findById(routeItem.getPlaceId()).orElseThrow(() -> new PlaceHandler(ErrorStatus.PLACE_NOT_FOUND));
+                    return RouteItemConverter.toRouteItem(routeItem.getItemOrder(), place);
+                }).toList();
+
+        Route route = RouteConverter.toRoute(request.getName(), routeItems);
+
+        routeRepository.save(route);
+        routeLikeRepository.save(RouteLikeConverter.toRouteLike(user, route));
+    }
 }
