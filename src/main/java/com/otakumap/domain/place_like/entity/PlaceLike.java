6d4f0efdd99fcb24552f6b6f1a930fee254f1679
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-02-07 20:00:57 +0900

    feat: PlaceLike 엔티티에 place 매핑 (place에만 있었어서 QueryDSL 생성이 안됨)

--- src/main/java/com/otakumap/domain/place_like/entity/PlaceLike.java
+++ src/main/java/com/otakumap/domain/place_like/entity/PlaceLike.java
@@ -1,6 +1,7 @@
 package com.otakumap.domain.place_like.entity;
 
 import com.otakumap.domain.mapping.PlaceAnimation;
+import com.otakumap.domain.place.entity.Place;
 import com.otakumap.domain.user.entity.User;
 import com.otakumap.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -24,6 +25,10 @@ public class PlaceLike extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
 
     @ManyToOne
     @JoinColumn(name = "place_animation_id", nullable = false)
