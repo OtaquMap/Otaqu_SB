b7f9c7796457ceff0189edf3d61d6d0455a986f5
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-02-07 15:24:25 +0900

    Chore: 오타 수정

--- src/main/java/com/otakumap/domain/place_like/controller/PlaceLikeController.java
+++ src/main/java/com/otakumap/domain/place_like/controller/PlaceLikeController.java
@@ -33,8 +33,8 @@ public class PlaceLikeController {
     @Operation(summary = "저장된 장소 목록 조회", description = "저장된 장소 목록을 불러옵니다.")
     @GetMapping("")
     @Parameters({
-            @Parameter(name = "lastId", description = "마지막으로 조회된 저장된 이벤트 id, 처음 가져올 때 -> 0"),
-            @Parameter(name = "limit", description = "한 번에 조회할 최대 이벤트 수. 기본값은 10입니다.")
+            @Parameter(name = "lastId", description = "마지막으로 조회된 저장된 장소 id, 처음 가져올 때 -> 0"),
+            @Parameter(name = "limit", description = "한 번에 조회할 최대 장소 수. 기본값은 10입니다.")
     })
     public ApiResponse<PlaceLikeResponseDTO.PlaceLikePreViewListDTO> getPlaceLikeList(@CurrentUser User user, @RequestParam(defaultValue = "0") Long lastId, @RequestParam(defaultValue = "10") int limit) {
         return ApiResponse.onSuccess(placeLikeQueryService.getPlaceLikeList(user, lastId, limit));
