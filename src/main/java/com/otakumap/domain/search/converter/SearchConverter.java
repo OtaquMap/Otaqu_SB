3d63a49ea30bc3c5e14c91ebaa8f9f216dfc5a3c
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-11 08:47:12 +0900

    Feat: toSearchResultDTO에 장소 정보 포함

--- src/main/java/com/otakumap/domain/search/converter/SearchConverter.java
+++ src/main/java/com/otakumap/domain/search/converter/SearchConverter.java
@@ -1,18 +1,26 @@
 package com.otakumap.domain.search.converter;
 
 import com.otakumap.domain.event.dto.EventResponseDTO;
+import com.otakumap.domain.place.DTO.PlaceResponseDTO;
 import com.otakumap.domain.search.dto.SearchResponseDTO;
 
 import java.util.List;
 
 public class SearchConverter {
 
-    public static SearchResponseDTO.SearchResultDTO toSearchResultDTO(List<EventResponseDTO.SearchedEventInfoDTO> eventDTOs, Double lat, Double lng) {
+    public static SearchResponseDTO.SearchResultDTO toSearchResultDTO(List<EventResponseDTO.SearchedEventInfoDTO> eventDTOs,
+                                                                      List<PlaceResponseDTO.SearchedPlaceInfoDTO> placeDTOs,
+                                                                      Double lat, Double lng) {
+        int placeAniCount = placeDTOs.stream()
+                .mapToInt(placeDto -> placeDto.getAnimations().size())
+                .sum();
+
         return SearchResponseDTO.SearchResultDTO.builder()
                 .latitude(lat)
                 .longitude(lng)
-                .eventCount(eventDTOs.size())
+                .count(eventDTOs.size() + placeAniCount)
                 .events(eventDTOs)
+                .places(placeDTOs)
                 .build();
     }
 }
