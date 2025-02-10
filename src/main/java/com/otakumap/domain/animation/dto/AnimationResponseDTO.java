c826b2c570fe5e30b8d3223b248fd3d0a331ed5f
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-11 08:30:24 +0900

    Feat: AnimationConverter, AnimationResponseDTO 구현

---
+++ src/main/java/com/otakumap/domain/animation/dto/AnimationResponseDTO.java
@@ -0,0 +1,22 @@
+package com.otakumap.domain.animation.dto;
+
+import com.otakumap.domain.hash_tag.dto.HashTagResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class AnimationResponseDTO {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AnimationInfoDTO {
+        private Long animationId;
+        private String animationName;
+        private Boolean isFavorite;
+        private List<HashTagResponseDTO.HashTagDTO> hashTags;
+    }
+}
