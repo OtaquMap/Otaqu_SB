7ad20ca11264254d4412a550d7ca2fd59a9c0b3f
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-13 03:35:43 +0900

    Feat: Service에서 PlaceAnimation을 기준으로 애니메이션 관련 해시태그를 가져오도록 수정

--- src/main/java/com/otakumap/domain/place_review/service/PlaceReviewQueryServiceImpl.java
+++ src/main/java/com/otakumap/domain/place_review/service/PlaceReviewQueryServiceImpl.java
@@ -1,6 +1,9 @@
 package com.otakumap.domain.place_review.service;
 
 import com.otakumap.domain.animation.entity.Animation;
+import com.otakumap.domain.hash_tag.converter.HashTagConverter;
+import com.otakumap.domain.hash_tag.dto.HashTagResponseDTO;
+import com.otakumap.domain.mapping.PlaceAnimation;
 import com.otakumap.domain.place.entity.Place;
 import com.otakumap.domain.place.repository.PlaceRepository;
 import com.otakumap.domain.place_review.converter.PlaceReviewConverter;
@@ -19,8 +22,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -56,12 +61,31 @@ public class PlaceReviewQueryServiceImpl implements PlaceReviewQueryService {
         List<PlaceReview> allReviews = routes.stream()
                 .map(placeReviewRepository::findAllByRoute).toList();
 
-        // 애니메이션별로 그룹화
+        // 애니메이션별 해시태그
+        List<PlaceAnimation> placeAnimations = allReviews.stream()
+                .map(PlaceReview::getPlaceAnimation)
+                .filter(Objects::nonNull)
+                .distinct()
+                .toList();
+        Map<Animation, List<HashTagResponseDTO.HashTagDTO>> animationHashTagMap =
+                placeAnimations.stream()
+                        .collect(Collectors.groupingBy(
+                                PlaceAnimation::getAnimation, // 그룹의 키 : PlaceAnimation이 참조하는 Animation
+
+                                Collectors.flatMapping(
+                                        pa -> pa.getPlaceAnimationHashTags().stream()
+                                                .peek(pah -> log.info("PlaceAnimation 정보 : {}, HashTag : {}", pa.getAnimation().getName(), pah.getHashTag().getName()))
+                                                .map(pah -> HashTagConverter.toHashTagDTO(pah.getHashTag())),
+                                        Collectors.toList()
+                                )
+                        ));
+
+        // 애니메이션별로 리뷰 그룹화
         Map<Animation, List<PlaceReview>> reviewsByAnimation = allReviews.stream()
                 .collect(Collectors.groupingBy(review -> review.getPlaceAnimation().getAnimation()));
 
         // 애니메이션 그룹마다 그 안에 속한 리뷰들 페이징 적용
-        List<PlaceReviewResponseDTO.AnimationReviewGroupDTO> animationGroups = paginateReviews(reviewsByAnimation, page, size);
+        List<PlaceReviewResponseDTO.AnimationReviewGroupDTO> animationGroups = paginateReviews(reviewsByAnimation, animationHashTagMap, page, size);
 
         // 총 리뷰 수 계산
         long totalReviews = reviewsByAnimation.values().stream()
@@ -71,17 +95,23 @@ public class PlaceReviewQueryServiceImpl implements PlaceReviewQueryService {
         return PlaceReviewConverter.toPlaceAnimationReviewDTO(place, totalReviews, animationGroups, finalAvgRating);
     }
 
-    private List<PlaceReviewResponseDTO.AnimationReviewGroupDTO> paginateReviews(Map<Animation, List<PlaceReview>> reviewsByAnimation, int page, int size) {
+    private List<PlaceReviewResponseDTO.AnimationReviewGroupDTO> paginateReviews(Map<Animation, List<PlaceReview>> reviewsByAnimation,
+                                                                                 Map<Animation, List<HashTagResponseDTO.HashTagDTO>> animationHashTagMap,
+                                                                                 int page, int size) {
 
         return  reviewsByAnimation.entrySet().stream()
                 .map(entry -> {
+                    Animation animation = entry.getKey();
                     List<PlaceReview> reviews = entry.getValue();
+                    // 각 애니메이션에 해당하는 해시태그 목록을 가져오기
+                    List<HashTagResponseDTO.HashTagDTO> hashTagsForAnimation = animationHashTagMap.getOrDefault(animation, Collections.emptyList());
+
                     int fromIndex = Math.min(page * size, reviews.size());
                     int toIndex = Math.min(fromIndex + size, reviews.size());
 
                     List<PlaceReview> pagedReviews = reviews.subList(fromIndex, toIndex);
 
-                    return PlaceReviewConverter.toAnimationReviewGroupDTO(entry.getKey(), pagedReviews);
+                    return PlaceReviewConverter.toAnimationReviewGroupDTO(animation, pagedReviews, hashTagsForAnimation);
                 })
                 .filter(group -> !group.getReviews().isEmpty()) // 빈 그룹 제외
                 .toList();
