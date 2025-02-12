82598084aa40fd67928d639f188d561af8de0faa
haerxeong <haeryeong04@naver.com>
2025-02-12 14:23:24 +0900

    Fix: 변경된 EventLocation 적용
    # Conflicts:
    #	src/main/java/com/otakumap/domain/event_location/repository/EventLocationRepository.java
    #	src/main/java/com/otakumap/domain/reviews/service/ReviewCommandServiceImpl.java

--- src/main/java/com/otakumap/domain/event_location/repository/EventLocationRepository.java
+++ src/main/java/com/otakumap/domain/event_location/repository/EventLocationRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EventLocationRepository extends JpaRepository<EventLocation, Long> {
+    boolean existsByLatAndLng(Double latitude, Double longitude);
     List<EventLocation> findByLatAndLng(Double latitude, Double longitude);
 }
